e089c443d38ba89dde7f67666804cce539575092
Opilane TTHK <opilane@tthk.ee>
2022-01-31 10:52:50 +0200

    Add project files.

---
+++ Uravnenie2_Tulusa.py
@@ -0,0 +1,155 @@
+from tkinter import *
+from math import *
+import matplotlib.pyplot as plt
+import numpy as np #[a,b]
+global D,t
+D=-1
+t="Нет решений!"
+
+def disc(a:float,b:float,c:float):
+    d=b**2-4*a*c
+    if d>0:
+        x1=(-b+sqrt(d))/(2*a)
+        x2=(-b-sqrt(d))/(2*a)
+    elif d==0:
+        x=-b/(2*a)
+    return x1,x2 or x
+
+def lahenda():    
+    if (a.get()!="" and b.get()!="" and c.get()!=""):
+        a_=int(a.get())
+        b_=int(b.get())
+        c_=int(c.get())
+        D=b_*b_-4*a_*c_
+        if D>0:
+            x1_=round((-1*b_+sqrt(D))/(2*a_),2)
+            x2_=round((-1*b_-sqrt(D))/(2*a_),2)
+            t=f"X1={x1_}, \nX2={x2_}"
+            flag=True
+        elif D==0:
+            x1_=round((-1*b_)/(2*a_),2)
+            t=f"X1={x1_}"
+            graf=True
+        else:
+            t="Корней нет"
+            graf=False
+        vastus.configure(text=f"D={D}\n{t}")
+        a.configure(bg="lightblue")
+        b.configure(bg="lightblue")
+        c.configure(bg="lightblue")
+    else:
+        if a.get()=="":
+            a.configure(bg="red")
+        if b.get()=="":
+            b.configure(bg="red")
+        if c.get()=="":
+            c.configure(bg="red")
+    return graf,D,t
+
+def graafik():
+    graf,D,t=lahenda()#nado lahenda dobavit
+    if graf==True:
+        a_=float(a.get())
+        b_=float(b.get())
+        c_=float(c.get())
+        x0=(-b_)/(2*a_)
+        y0=a_*x0*x0+b_*x0+c_
+        x1=np.arange(x0-10,x0+10,0.5)#min max step [min,max]
+        y1=a_*x1*x1+b_*x1+c_
+        fig = plt.figure()
+        plt.plot(x0,y0,x1,y1,"r-d")
+        plt.title("Квадратное уравнение")
+        plt.ylabel("y")
+        plt.xlabel("x")
+        plt.grid(True)
+        plt.show()
+        text=f"Вершина параболы ({x0},{y0})"
+    else:
+        text=f"Гравик нет возможности построить"
+    vastus.configure(text=f"D={D}\n{t}\n{text}")
+
+t=0
+def veel():
+    global t
+    if t==0:
+        aken.geometry(str(aken.winfo_width())+"x"+str(aken.winfo_height()+200))
+        btn_veel.config(text="Уменьшить окно")
+        t=1
+    else:
+        aken.geometry(str(aken.winfo_width())+"x"+str(aken.winfo_height()-200))
+        btn_veel.config(text="Увеличить окно")
+        t=0
+
+def kala():
+    x1 = np.arange(0, 9.5, 0.5)#min max step
+    y1=(2/27)*x1*x1-3
+    x2 = np.arange(-10, 0.5, 0.5)
+    y2=0.04*x2*x2-3
+    x3 = np.arange(-9, -3.5, 0.5)
+    y3=(2/9)*(x3+6)**2+1
+    x4 = np.arange(-3, 9.5,  0.5)
+    x4=(-1/12)-(x4-3)**2+6
+    x5 = np.arange(5, 9.5, 0.5)
+    y5=(1/9)*(x5*5)**2+2
+    x6 = np.arange(5, 8.5, 0,5)
+    y6=(1/8)*(x6-7)**2+1.5
+    x7 = np.arange(-13, 8.5, 0.5)
+    y7=(-0.75)*(x7+11)**2+6
+    x8 = np.arange(-15, -12.5, 0.5)
+    y8=(-0.5)*(x8+13)**2+3
+    x9 = np.arange(-15, -10, 0.5)
+    y9=[1]*len(x9)
+    x10 = np.arange(3, 4, 0.5)
+    y10=[3]*len(x10)
+    fig = plt.figure()
+    plt.plot(x1,y1,x2,y2,x3,y3.x4,y4,x5,y5,x6,y6,x7,y7,x8,y8,x9,y9,x10,y10)
+    plt.title("Кит")
+    plt.ylabel("y")
+
+
+aken=Tk()
+aken.title("Квадратное уравнение")
+aken.geometry("700x200")
+f1=Frame(aken,width=650,height=200)
+f1.pack(side=TOP)
+f2=Frame(aken,width=650,height=200)
+f2.pack(side=BOTTOM)
+
+zagolovok=Label(f1,text="Решение квадратного уравнения",font="Arial 20",width=30,bg="#B0E0E6",fg="#2E8B57",relief=RAISED)
+zagolovok.pack()
+
+grafik=Button(f1,text="График",font="Arial 20",bg="green",command=graafik)
+grafik.pack(side=RIGHT)
+reshit=Button(f1,text="Решить",font="Arial 20",width=7,bg="green")
+reshit.pack(side=RIGHT)
+
+x2=Entry(f1,width=3,font="Arial 20",fg="blue",bg="lightblue", justify=CENTER)#x v kvadrate
+x2.pack(side=LEFT)
+
+xkvadrat=Label(f1,text="x**2+",font="Arial 20",width=5,fg="#2E8B57")
+xkvadrat.pack(side=LEFT)
+
+x=Entry(f1,width=4,font="Arial 20",fg="blue",bg="lightblue", justify=CENTER)#x
+x.pack(side=LEFT)
+
+prosto_x=Label(f1,text="x+",font="Arial 20",width=3,fg="#2E8B57")
+prosto_x.pack(side=LEFT)
+
+cislo=Entry(f1,width=3,font="Arial 20",fg="blue",bg="lightblue", justify=CENTER)
+cislo.pack(side=LEFT)
+
+btn_veel=Button(f2,text="Увеличить окно",font="Calibri 26",bg="green",command=veel)
+btn_veel.pack(side=TOP)
+var=IntVar()
+r1=Radiobutton(f2,text="Кит",variable=var,var=1,font="Calibri 26")#,command=kala
+r2=Radiobutton(f2,text="Очки",variable=var,var=2,font="Calibri 26")
+r3=Radiobutton(f2,text="Лягуха",variable=var,var=3,font="Calibri 26")
+r1.pack()
+r2.pack()
+r3.pack()
+
+#resenie=Label(f1,text="Решение",font="Arial 20",width=150,bg="yellow",fg="black",relief=RAISED)
+#resenie.geometry=("120x120") nado kak-to izmenit razmer
+#resenie.place(x=50,y=250)
+
+aken.mainloop()
